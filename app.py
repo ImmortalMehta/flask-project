62c24587b79da530edebe3ec0f980b16ccb0235b
Tutedude <mwhtachiranjiv05@gmail.com>
2026-01-08 11:08:46 +0530

    Initial commit: Flask MongoDB project

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://immortal:immortalduck@gooner.fnw1yio.mongodb.net/?appName=gooner")
+db = client["mydatabase"]
+collection = db["mycollection"]
+
+@app.route('/')
+def form():
+    return render_template('entry.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    try:
+        name = request.form['name']
+        email = request.form['email']
+
+        # Insert into MongoDB Atlas
+        collection.insert_one({"name": name, "email": email})
+
+        # Redirect to success page
+        return redirect(url_for('success'))
+
+    except Exception as e:
+        # Show error on same page
+        return render_template('entry.html', error=str(e))
+
+@app.route('/success')
+def success():
+    return render_template('out.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
